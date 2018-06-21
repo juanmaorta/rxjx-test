9127a184e14a12915cf09f1c7f09296246a5287e
Juanma Orta <juanma.orta@gmail.com>
2018-06-21 10:28:59 +0200

    Webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import _ from 'lodash'
+import Rx from 'rxjs/Rx'
 
-import { Subject } from 'rxjs/Subject'
-const mySubject = new Subject()
+const mySubject = new Rx.Subject()
 
 const observerObj = {
   next: (o) => displayText(`observer A: ${o}`)
@@ -11,17 +10,26 @@ const displayText = (text) => {
   document.body.appendChild(component(text))
 }
 
+const component = (text) => {
+  var element = document.createElement('div')
+  element.innerHTML = text
+
+  return element
+}
+
+/**
 mySubject.subscribe(observerObj)
 mySubject.subscribe({
   next: o => displayText(`observer B: ${o}`)
 })
 mySubject.next(1)
 mySubject.next(2)
+*/
 
-function component (text) {
-  var element = document.createElement('div')
- // Lodash, now imported by this script
-  element.innerHTML = text
-
-  return element
-}
+const node = document.querySelector('input[type=text]')
+const input$ = Rx.Observable.fromEvent(node, 'input')
+  .map(event => event.target.value)
+  .filter(value => value.length >= 3)
+  .subscribe( value => {
+    next: displayText(`You just typed: ${value}!`)
+  })
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,16 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true
   }
-};
+}
